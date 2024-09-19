d889c8b8a3b4db4a56066de498a388683ec2f906
kimtaehan11 <kimtaehan11@gmail.com>
2024-09-19 14:25:08 +0900

    first commit

---
+++ src/test/java/developx/book_report/BookReportApplicationTests.java
@@ -0,0 +1,13 @@
+package developx.book_report;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookReportApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
