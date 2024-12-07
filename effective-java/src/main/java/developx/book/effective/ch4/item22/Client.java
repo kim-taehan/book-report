25c3d6c25b04c385441c0859c084f8564009a246
kimtaehan11 <kimtaehan11@gmail.com>
2024-12-07 21:21:14 +0900

    ## 4장 클래스와 인터페이스

---
+++ effective-java/src/main/java/developx/book/effective/ch4/item22/Client.java
@@ -0,0 +1,8 @@
+package developx.book.effective.ch4.item22;
+
+public class Client {
+
+    public static void main(String[] args) {
+        System.out.println("PhysicalConstants.AVOGADROS_NUM = " + PhysicalConstants.AVOGADROS_NUM);
+    }
+}
---
+++ effective-java/src/main/java/developx/book/effective/ch4/item22/PhysicalConstants.java
@@ -0,0 +1,5 @@
+package developx.book.effective.ch4.item22;
+
+public interface PhysicalConstants {
+    static final double AVOGADROS_NUM = 6.022_140_857e23;
+}
