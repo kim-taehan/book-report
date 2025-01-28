fe6bfb6b29d9d4058fcf76bb68e8ccf2a84da130
kimtaehan11 <kimtaehan11@gmail.com>
2025-01-29 04:35:35 +0900

    JPA 책 시작

--- jpa/README.md
+++ jpa/README.md
@@ -2,3 +2,5 @@
 # 자바 ORM 표준 JPA 프로그래밍 
 
 ## [1장 JPA 소개](src%2Fmain%2Fjava%2Fdevelopx%2Fbook%2Fjpa%2Fch1%2Fch1.md)
+
+## [ch2.md](src%2Fmain%2Fjava%2Fdevelopx%2Fbook%2Fjpa%2Fch2%2Fch2.md)
---
+++ jpa/src/main/java/developx/book/jpa/ch2/Member.java
@@ -0,0 +1,23 @@
+package developx.book.jpa.ch2;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Table(name = "MEMBER")
+@Getter
+@Setter
+public class Member {
+
+    @Id
+    @Column(name = "ID")
+    private String id;
+
+    @Column(name = "NAME")
+    private String username;
+    private Integer age;
+}
